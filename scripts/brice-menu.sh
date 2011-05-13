596208ac3d2bceb3589ee72f012a96cd34bfd3db
Brice Fernandes <bfer@BHAC.(none)>
2011-05-13 14:22:11 +0100

    Added caching for brice-menu to avoid delays.

--- scripts/brice-menu.sh
+++ scripts/brice-menu.sh
@@ -2,8 +2,25 @@
 
 source /home/$(whoami)/scripts/ems-env.sh
 PATH=$PATH:/home/$(whoami)/scripts
+CACHEFILE="/home/$(whoami)/.config/brice-menu.cache"
 
-CMD=`ls $(echo $PATH | tr ':' ' ')  | grep -v "/:" | sort | uniq | dmenu -i -b -nb black -nf white -sb red -sf white -fn `
+reload (){
+  ls $(echo $PATH | tr ':' ' ') | grep -v "/:" | sort | uniq > $CACHEFILE
+}
+
+test ! -d /home/$(whoami)/.config/ && mkdir /home/$(whoami)/.config/
+test ! -f $CACHEFILE  && reload
+
+if [ "$1" == "-r" ]; then
+  reload;
+fi
+
+if [ "$1" == "--reload-only" ]; then
+  reload;
+  exit;
+fi
+
+CMD=`cat $CACHEFILE | dmenu -i -b -nb black -nf white -sb red -sf white -fn `
 
 $CMD
 
