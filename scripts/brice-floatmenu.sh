7a64a59474b9e02ce702325269d7fcac53f971c7
Brice Fernandes <bfer@BHAC.(none)>
2011-06-02 13:42:21 +0100

    dzen2 menu. requires latest build.

---
+++ scripts/brice-floatmenu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# NOTE WELL:
+#   Requires the latest development version of dzen2 for all the features to work.
+#
+
+EVENTS="onstart=uncollapse,scrollhome;entertitle=uncollapse,grabkeys;\
+enterslave=grabkeys;leaveslave=exit,ungrabkeys;\
+button1=menuexec;button2=togglestick;button3=exit:13;\
+button4=scrollup;button5=scrolldown;\
+key_Down=scrolldown;keyUp=scrollup;key_Page_Down=scrolldown:30;key_Page_Up=scrollup:30;\
+key_Escape=ungrabkeys,exit"
+
+LINES=12
+
+DZEN="/home/$(whoami)/Downloads/dzen/dzen2"
+
+XP=$(getcurpos | cut -c1-4 ) 
+YP=$(getcurpos | cut -c5-10)
+
+
+(echo "^fg(red)MENU^fg()"; cat  ~/.config/brice-menu.cache) | $DZEN -ta c -sa c -l $LINES -p -e $EVENTS -x $XP -y $YP -tw 100 -w 100 -m 
+
---
+++ scripts/getcurpos.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# uses the package python-xlib
+# from http://snipplr.com/view/19188/mouseposition-on-linux-via-xlib/
+
+from Xlib import display
+import sys
+
+def mousepos():
+    data = display.Display().screen().root.query_pointer()._data
+    return data["root_x"], data["root_y"]
+
+if __name__ == "__main__":
+    x,y=mousepos()
+    if (sys.argv[1] == "-x"):
+      print("%d"%x)
+    elif (sys.argv[1] == "-y"):
+      print("%d"%y)
