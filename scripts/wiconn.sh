05bd99581c3863badcf2a1dfb22aae23ca0c4d25
Brice Fernandes <brice.fernandes@gmail.com>
2011-06-27 22:37:10 +0100

    minor changes

--- scripts/wiconn.sh
+++ scripts/wiconn.sh
@@ -5,30 +5,35 @@ col2="\e[1;34m"
 
 ESSID=$1
 
+LOGFILE=~/.wpa.log
+
 MAC_H=00:18:de:39:0a:a0
 
-echo -e "${col2}::${col1} Killing existing processes\e[m"
-sudo killall wpa_supplicant &>> /home/brice/wpa.log
-sudo killall dhcpcd &>> /home/brice/wpa.log
+tell (){
+  echo -e "${col2}::${col1} $@\e[m"
+}
 
-echo -e "${col2}::${col1} Bringing down wlan0\e[m"
+sudo killall wpa_supplicant &> $LOGFILE
+sudo killall dhcpcd &>> $LOGFILE
+
+tell Bringing down wlan
 sudo ifconfig wlan0 down
 
-echo -e "${col2}::${col1} Correcting MAC address\e[m"
+tell Correcting MAC address
 sudo ifconfig wlan0 hw ether $MAC_H
 
-echo -e "${col2}::${col1} Bringing wlan0 up\e[m"
+tell Bringing wlan0 up
 sudo ifconfig wlan0 up
 
-echo -e "${col2}::${col1} Associating with essid\e[m"
+tell Associating with essid
 sudo iwconfig wlan0 essid $ESSID
 
-echo -e "${col2}::${col1} Running wpa_supplicant\e[m"
-sudo wpa_supplicant -Dwext -iwlan0 -c/home/brice/wpa_supplicant.conf >> /home/brice/wpa.log &
+tell Running wpa_supplicant
+sudo wpa_supplicant -Dwext -iwlan0 -c/etc/wpa_supplicant.conf >> $LOGFILE &
 
 sleep 3
 
-echo -e "${col2}::${col1} Requesting IP via DHCP\e[m"
+tell Requesting IP via DHCP
 sudo dhcpcd wlan0 
 
 
