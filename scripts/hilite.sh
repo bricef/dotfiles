52f7a5d3bb2f56b3dbdd90e76d422bd465467541
Brice Fernandes <bfer@cbnl.com>
2011-09-26 10:29:49 +0100

    Adding MIT license to script (suitable for work use).

--- scripts/hilite.sh
+++ scripts/hilite.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# (c) 2011 by Brice fernandes <brice.fernandes@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Source available at https://github.com/bricef/dotfiles/blob/master/scripts/hilite.sh
+
+B_BLACK="\x1b[40m"
 B_RED="\x1b[41m"
 B_GREEN="\x1b[42m"
 B_YELLOW="\x1b[43m"
@@ -8,6 +31,7 @@ B_MAGENTA="\x1b[45m"
 B_CYAN="\x1b[46m"
 B_WHITE="\x1b[47m"
 
+F_BLACK="\x1b[30m"
 F_RED="\x1b[31m"
 F_GREEN="\x1b[32m"
 F_YELLOW="\x1b[33m"
@@ -29,6 +53,7 @@ post=$END
 
 color_fg(){
 	case $1 in
+		black)pre=$pre$F_BLACK;;
 		red)pre=$pre$F_RED;;
 		green)pre=$pre$F_GREEN;;
 		yellow)pre=$pre$F_YELLOW;;
@@ -36,12 +61,13 @@ color_fg(){
 		magenta)pre=$pre$F_MAGENTA;;
 		cyan)pre=$pre$F_CYAN;;
 		white)pre=$pre$F_WHITE;;
-		*) echo "'$1' is not a color!">/dev/sdterr; exit 1;
+		*) echo "'$1' is not a color!" 1>&2 ; exit 1;
 	esac
 }
 
 color_bg(){
 	case $1 in
+		black)pre=$pre$B_BLACK;;
 		red)pre=$pre$B_RED;;
 		green)pre=$pre$B_GREEN;;
 		yellow)pre=$pre$B_YELLOW;;
@@ -49,7 +75,7 @@ color_bg(){
 		magenta)pre=$pre$B_MAGENTA;;
 		cyan)pre=$pre$B_CYAN;;
 		white)pre=$pre$B_WHITE;;
-		*) echo "'$1' is not a color!">/dev/sdterr; exit 1;
+		*) echo "'$1' is not a color!" 1>&2 ; exit 1;
 	esac
 }
 
@@ -64,8 +90,8 @@ usage(){
 	echo "-B          Make text bold"
   echo ""
 	echo "Colors can be any of 'red' 'green' 'yellow' 'blue' 'magenta' 'cyan'"
-	echo "'white'. The default is a bold red foreground on default background"
-  echo "which is equivalent to:"
+	echo "'white' and 'black'. The default is a bold red foreground on default"
+  echo "background which is equivalent to:"
   echo "    hilite.sh -B -f red "
 }
 
@@ -90,4 +116,4 @@ if [ ! -n "$pre" ]; then
   pre=${F_RED}
 fi
 
-sed "s/$regex/$pre$bold&$post/g" < /dev/stdin >/dev/stdout
+cat - | sed "s/$regex/$pre$bold&$post/g"
