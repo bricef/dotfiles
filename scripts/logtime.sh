a17931ba2671b62c38dfd8724b36bf38bfc4d84d
Brice Fernandes <bfer@cbnl.com>
2011-10-21 13:57:26 +0100

    show current activity for logtime

--- scripts/logtime.sh
+++ scripts/logtime.sh
@@ -7,19 +7,23 @@ test ! -d $LOGDIR && mkdir -p $LOGDIR
 LOGFILE=$LOGDIR/$(date "+%Y%m%d.log")
 ACTIVFILE=/home/$(whoami)/.activities
 
+if [ "$1" = "show" ]; then
 
-prefix=$(date "+[%Y-%m-%d %H:%M]: ")
+  tail -1 $LOGFILE
 
-ACTIVITY=$(cat $ACTIVFILE | dmenu -b )
+else
 
-if [ "$ACTIVITY" ]; then
-  if [ "$ACTIVITY" = "@show" ]; then
-    zenity --warning --text="Operation not currently supported"
-  else
-    echo $prefix $ACTIVITY>> $LOGFILE
-    cat <(echo $ACTIVITY) $ACTIVFILE | sort | uniq > $ACTIVFILE.tmp
-    mv $ACTIVFILE.tmp $ACTIVFILE
+  prefix=$(date "+[%Y-%m-%d %H:%M]: ")
+  ACTIVITY=$(cat $ACTIVFILE | dmenu -b )
+  if [ "$ACTIVITY" ]; then
+    if [ "$ACTIVITY" = "@show" ]; then
+      zenity --warning --text="Operation not currently supported"
+    else
+      echo $prefix $ACTIVITY>> $LOGFILE
+      cat <(echo $ACTIVITY) $ACTIVFILE | sort | uniq > $ACTIVFILE.tmp
+      mv $ACTIVFILE.tmp $ACTIVFILE
+    fi
   fi
-fi
 
+fi
 
