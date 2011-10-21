d6948cf7ae95551811aca2ce31700cbde37e52c0
Brice Fernandes <bfer@cbnl.com>
2011-10-21 18:00:20 +0100

    time logger show output

--- scripts/logtime.sh
+++ scripts/logtime.sh
@@ -8,10 +8,12 @@ LOGFILE=$LOGDIR/$(date "+%Y%m%d.log")
 ACTIVFILE=/home/$(whoami)/.activities
 
 if [ "$1" = "show" ]; then
+  
+  length=$(wc -l $LOGFILE| awk '{print $1}')
+  cat <(echo "(press Return to exit)") $LOGFILE \
+    | dzen2 -e 'onstart=uncollapse,grabkeys,grabmouse;key_Return=exit' -l $length -p -fn '-*-fixed-medium-r-*-*-13-*-*-*-*-*-*-*' -tw 500 -x 710 -y 100 -w 500 -bg grey -fg black -h 20
 
-  tail -1 $LOGFILE
-
-else
+elif [ "$1" = "record" ]; then
 
   prefix=$(date "+[%Y-%m-%d %H:%M]: ")
   ACTIVITY=$(cat $ACTIVFILE | dmenu -b )
