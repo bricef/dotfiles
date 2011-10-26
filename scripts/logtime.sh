2457944b17e2ed3f5395f81a94ae3ae3007a8d56
Brice Fernandes <bfer@cbnl.com>
2011-10-26 15:11:11 +0100

    cleaner logtime

--- scripts/logtime.sh
+++ scripts/logtime.sh
@@ -8,17 +8,24 @@ LOGFILE=$LOGDIR/$(date "+%Y%m%d.log")
 ACTIVFILE=/home/$(whoami)/.activities
 
 prefix=$(date "+[%Y-%m-%d %H:%M]: ")
-ACTIVITY=$(cat <(echo "@show") $ACTIVFILE | dmenu -b )
-if [ "$ACTIVITY" ]; then
-  if [ "$ACTIVITY" = "@show" ]; then
+ACTIVITY=$(cat <(echo -e "@show\n@edit") $ACTIVFILE | dmenu -b )
+
+
+case "$ACTIVITY" in 
+  "@show")
     length=$(wc -l $LOGFILE| awk '{print $1}')
     cat <(echo "(press Esc/Return to exit)") $LOGFILE \
       | dzen2 -e 'onstart=uncollapse,grabkeys,grabmouse;key_Return=exit;key_Escape=exit' -l $length -p -fn '-*-fixed-medium-r-*-*-13-*-*-*-*-*-*-*' -tw 500 -x 710 -y 100 -w 500 -bg grey -fg black -h 20
-  elif [ "$ACTIVITY" = "@edit" ]; then
+    ;;
+  "@edit")
     gnome-terminal --working-directory=/home/$(whoami)/ -e "$EDITOR $(readlink -f $LOGFILE)"
-  else
+    ;;
+  "")
+    #do nothing
+    ;;
+  *)
     echo $prefix $ACTIVITY>> $LOGFILE
     cat <(echo $ACTIVITY) $ACTIVFILE | sort | uniq > $ACTIVFILE.tmp
     mv $ACTIVFILE.tmp $ACTIVFILE
-  fi
-fi
+    ;;
+esac
