eae96f1005fb536ea7b861443101c1aaa07aed38
Brice Fernandes <bfer@cbnl.com>
2011-10-31 10:18:11 +0000

    minor

--- scripts/top-left.sh
+++ scripts/top-left.sh
@@ -4,6 +4,6 @@
 while true; do   
   echo "--"
   sleep 1
-done | dzen2 -p -tw 350 -x 0 -y 0 -ta r -fn '-*-fixed-medium-r-*-*-13-*-*-*-*-*-*-*' -fg "#ffffff" -bg "#000000"
+done | dzen2 -p -tw 350 -x 0 -y 0 -ta l -fn '-*-fixed-medium-r-*-*-13-*-*-*-*-*-*-*' -fg "#ffffff" -bg "#000000"
   
 
