1aa49174a23c051bc34fe19daf4a859241fc0c89
Brice Fernandes <bfer@cbnl.com>
2011-11-14 11:21:17 +0000

    winlist now highlights active window

--- scripts/winlist.sh
+++ scripts/winlist.sh
@@ -3,13 +3,22 @@
 
 while true; do   
   DESKTOP=$(wmctrl -d | grep "\*" | awk '{print $1}')
+  ACTIVE="0x0$(echo "obase=16;`xdotool getactivewindow`" | bc | tr '[:upper:]' '[:lower:]')"
+  
   wmctrl -l \
-  | awk -v desk=$DESKTOP '{if($2 == desk) print substr($0, index($0,$4)) }' \
+  | awk -v desk=$DESKTOP '{if($2 == desk) print $0}'\
   | while read line; do
-      echo -n "| $(echo $line| sed 's|\(.\{10\}\).*|\1|g') "
+    WINID=$(echo $line | awk '{print $1}' )
+    TITLE=$(echo $line | awk '{print substr($0, index($0,$4))}' | sed 's|\(.\{10\}\).*|\1|g'  )
+    if [ "$ACTIVE" = "$WINID" ]; then
+      echo -n "-^bg(red) $TITLE ^bg(black)"
+    else 
+      echo -n "- $TITLE "
+    fi
     done
-    echo "|"
+    echo "-"
   sleep 1
  done | dzen2 -p -tw 1220 -x 350 -y 0 -ta c -fn '-*-fixed-medium-r-*-*-13-*-*-*-*-*-*-*' -fg "#ffffff" -bg "#000000"
-  
+ 
+
 
