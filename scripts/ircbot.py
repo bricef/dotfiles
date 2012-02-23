47d89f04afd45cfa786ffd1bf99f5834ad835fcd
Brice Fernandes <bfer@cbnl.com>
2012-02-23 18:12:07 +0000

    join eng

--- scripts/ircbot.py
+++ scripts/ircbot.py
@@ -1,27 +1,30 @@
 import sys
 import socket
 import string
+import time
 
-HOST="irc.freenode.net"
+HOST="engbot"
 PORT=6667
-NICK="MauBot"
-IDENT="maubot"
-REALNAME="MauritsBot"
+NICK="loggly"
+IDENT="loggly"
+REALNAME="logglybot"
 readbuffer=""
 
 s=socket.socket( )
 s.connect((HOST, PORT))
-s.send("NICK %s\r\n" % NICK)
 s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+s.send("NICK %s\r\n" % NICK)
+time.sleep(2)
+s.send("JOIN #eng \r\n")
 
 while 1:
-    readbuffer=readbuffer+s.recv(1024)
+    readbuffer=readbuffer+s.recv(2048)
     temp=string.split(readbuffer, "\n")
     readbuffer=temp.pop( )
 
     for line in temp:
         line=string.rstrip(line)
-        line=string.split(line)
         print line
+        line=string.split(line)
         if(line[0]=="PING"):
             s.send("PONG %s\r\n" % line[1])
