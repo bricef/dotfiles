77793a4a3b2e27813e35e5a65b68342fdd17d664
Brice Fernandes <bfer@cbnl.com>
2012-09-02 21:07:07 +0100

    Raven specific changes

--- scripts/status.sh
+++ scripts/status.sh
@@ -23,11 +23,14 @@ netstatus(){
 
 
 status() {
-    IP_eth1=`netstatus eth1 2>/dev/null`
     IP_eth0=`netstatus eth0 2>/dev/null`
+    IP_wlan0=`netstatus wlan0 2>/dev/null`
 
-    echo '| eth0:' $IP_eth0 \
-    '| eth1:' $IP_eth1 \
+    echo '|' $(dropbox status) \
+    '| vol:' $(~/scripts/volch.sh -q) \
+    '|' $(sensors | grep temp1 | awk '{print $2}' | tr -d "\n" | tr "+" " ") \
+    '| eth0:' $IP_eth0 \
+    '| wlan0:' $IP_wlan0 \
     '| up:' $(uptime | tr -d ','|awk '{print $3" "$4" "$5}') \
     '|' $(date +"%a %d %b, %H:%M") '|' 
 }
@@ -39,7 +42,7 @@ elif [[ $1 == --dzen ]]; then
     while true; do 
         status
         sleep 1
-    done | dzen2 -tw 1920 -x 1920 -ta r -fn '-*-fixed-medium-r-*-*-13-*-*-*-*-*-*-*' -y 1065 -fg "#ffffff" -bg "#000000" # "*terminus*medium*-14-*"
+    done | dzen2 -tw 1680 -x 1680 -ta r -y 1035 -fg "#ffffff" -bg "#000000" # "*terminus*medium*-14-*"
 else
     echo "invoke with --string or --dzen"
 fi;
